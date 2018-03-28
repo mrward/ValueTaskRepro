7622bc35cead4e8ae040773c3e9dab05cafdcdf2
Brandon Minnick <brandon.r.minnick@gmail.com>
2018-03-27 20:38:10 -0700

    First Commit

---
+++ Droid/Path_Android.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ValueTaskRepro.Droid
+{
+    public class Path_Android : IPath
+    {
+        string _path;
+
+        public ValueTask<string> GetPath()
+        {
+            if (string.IsNullOrWhiteSpace(_path))
+                return new ValueTask<string>(_path);
+
+            return new ValueTask<string>(GetPathTask());         
+        }
+
+        async Task<string> GetPathTask()
+        {
+            _path = await Task.Run(() => Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+            return _path;
+        }
+    }
+}
---
+++ ValueTaskRepro/App.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ValueTaskRepro
+{
+	public class App : Application
+	{
+		public App() => MainPage = new ContentPage();
+	}
+
+    public interface IPath
+	{
+		ValueTask<string> GetPath();
+	}
+}
